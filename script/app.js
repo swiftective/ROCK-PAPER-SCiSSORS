c94cf6f2d99f0fc21929050b2ae0f89fa9d3e273
Sashane RV <detectivesherlockholmes4u@gmail.com>
2021-11-06 22:56:33 +0530

    Add humanPlay() and computerPlay() functions

--- script/app.js
+++ script/app.js
@@ -1,4 +1,3 @@
-
 console.log(
   "%c Type" +
     "%c game()" +
@@ -8,6 +7,37 @@ console.log(
   "color: white; font-style: normal;"
 );
 
-game = () => {
-  console.log("Still in Progress")
+function game() {
+  // let gameRound = 0;
+  for (let i = 0, len = 5; i < len; i++) {
+    // gameRound = ++gameRound;
+    // console.log(`This is the ${gameRound} round !!!`)
+    const playerSelection = humanPlay();
+    const computerSelection = computerPlay();
+    let roundScore = playRound(playerSelection, computerSelection);
+    if (roundScore == 'computer') {
+    }
+  }
+}
+
+// random computer command
+function computerPlay() {
+  let optionArray = ["rock", "paper", "scissors"];
+  let randomNum = Math.floor(Math.random() * optionArray.length);
+  return optionArray[randomNum];
+}
+
+// sanitized user input for command
+function humanPlay() {
+  let optionArray = ["rock", "paper", "scissors"];
+  var playerSelection = prompt("Input command:");
+  playerSelection = playerSelection.toLowerCase();
+  while (optionArray.includes(playerSelection) == false){
+    playerSelection = prompt("Error Try again:");
+    playerSelection = playerSelection.toLowerCase();
+  }
+  return playerSelection;
+}
+
+function playRound(player1, player2) {
 }
