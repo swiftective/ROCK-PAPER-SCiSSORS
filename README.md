ecd3230ab80fc0db6dccb51852589b63f06569aa
Sashane RV <detectivesherlockholmes4u@gmail.com>
2021-11-06 19:01:22 +0530

    Added webpage interface

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ROCK-PAPER-SCiSSORS
+# ROCK-PAPER-SCiSSORS 🎇
 
 ## The Odin Project Assignment
 
--- script/app.js
+++ script/app.js
@@ -1,2 +1,13 @@
 
-console.log("Good")
+console.log(
+  "%c Type" +
+    "%c game()" +
+    "%c on the console and press Enter to start the game !!!",
+  "color: white;",
+  "color: #48ab61; font-style: italic;",
+  "color: white; font-style: normal;"
+);
+
+game = () => {
+  console.log("Still in Progress")
+}
